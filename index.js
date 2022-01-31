23a08fbd1f135243a17df32ce52507e6879948b3
Geremsa Daimary <geremsa.daimary@datoms.io>
2022-01-31 11:54:36 +0530

    project setup

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import chalkAnimation from 'chalk-animation';
+import gradient from 'gradient-string';
+import figlet from 'figlet';
+import inquirer from 'inquirer';
+
+console.log( chalk.bgMagenta("Hello CLI!"));
